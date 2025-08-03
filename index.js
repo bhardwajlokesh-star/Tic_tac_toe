520675de20f99c955259cf9135dad3a839613776
bhardwajlokesh-star <lokeshbhardwaj978360448@gmail.com>
2025-08-03 11:54:18 +0000

    Add: Connect DOM and all the buttons with js

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ let board =[["","",""],
 
 let currentPlayer="X";
 
+document.addEventListener('DOMContentLoaded',()=>{
 
-function makeMove(index){
-    if(board[index]===""){
-        board[index]=currentPlayer;
-    }
-}
+    const vsPlayer = document.getElementById('Player');
+    const welcomeScreen=document.getElementById("container");
+    const gameSection=document.getElementById("Game");
+    const xButton=document.getElementById("X");
+    const oButton=document.getElementById("O");
+    const gameBoard=document.getElementById("game-board");
+
+
+    gameSection.style.display='none';
+    gameBoard.style.display='none';
+
+
+    vsPlayer.addEventListener('click',() =>{
+        welcomeScreen.style.display='none';
+        gameSection.style.display='flex'
+    })
+});
+
+
